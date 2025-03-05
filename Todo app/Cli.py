9aad8ad4a018668bb7b3f201841921415310a412
Igor Caponi <icaponi@hotmail.com>
2025-03-05 12:31:39 -0300

    Adicionado functions.py

--- Todo app/Cli.py
+++ Todo app/Cli.py
@@ -1,51 +1,6 @@
-class TodoApp:
-    def __init__(self, filename="todos.txt"):
-        self.filename = filename
-        self.todos = self.load_todos()
-
-    def load_todos(self):
-        """Load todos from file, create file if it doesn't exist"""
-        try:
-            with open(self.filename, 'r') as f:
-                return [line.strip() for line in f.readlines()]
-        except FileNotFoundError:
-            return []
-
-    def save_todos(self):
-        """Save todos to file"""
-        with open(self.filename, 'w') as f:
-            for todo in self.todos:
-                f.write(f"{todo}\n")
-
-    def add_todo(self, task):
-        """Add a new todo"""
-        self.todos.append(task)
-        self.save_todos()
-        print(f"Added: {task}")
-
-    def remove_todo(self, index):
-        """Remove a todo by index"""
-        try:
-            removed_task = self.todos.pop(index - 1)
-            self.save_todos()
-            print(f"Removed: {removed_task}")
-        except IndexError:
-            print("Invalid task number!")
-
-    def list_todos(self):
-        """Display all todos"""
-        if not self.todos:
-            print("No tasks in your TODO list!")
-            return
-        
-        print("\nYour TODO List:")
-        print("-" * 40)
-        for i, task in enumerate(self.todos, 1):
-            print(f"{i}. {task}")
-        print("-" * 40)
-
+import functions
 def main():
-    app = TodoApp()
+    app = functions.TodoApp()
     
     while True:
         print("\nTODO App Menu:")
