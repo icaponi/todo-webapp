5a784dc49a07e7cbb661bfd30086f4b3c634bec3
Igor Caponi <icaponi@hotmail.com>
2025-03-06 19:55:26 -0300

    primeiro app

--- Todo app/gui.py
+++ Todo app/gui.py
@@ -1,4 +1,13 @@
 import functions
 
-import FreeSimpleGUI
-FreeSimpleGUI.Window("My To-Do App")
+import FreeSimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box=sg.InputText(tooltip="Enter todo")
+add_button=sg.Button("Add")
+edit_button =sg.Button("Edit")
+complete_button =sg.Button("Complete/Remove")
+
+window = sg.Window('My To-Do App',layout=[[label], [input_box,add_button]])
+window.read()
+window.close()
