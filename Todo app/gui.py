b206db16f99638425add23970403cee33bb0f942
Igor Caponi <icaponi@hotmail.com>
2025-03-09 12:58:26 -0300

    Implement GUI functionality for Todo app with add, edit, and list features

--- Todo app/gui.py
+++ Todo app/gui.py
@@ -2,13 +2,25 @@ import functions
 
 import FreeSimpleGUI as sg
 
+app=functions.TodoApp()
 label = sg.Text("Type in a to-do")
-input_box=sg.InputText(tooltip="Enter todo")
+input_box=sg.InputText(tooltip="Enter todo", key = 'todo')
 add_button=sg.Button("Add")
 edit_button =sg.Button("Edit")
 complete_button =sg.Button("Complete/Remove")
-teste=sg.Checkbox("")
 
-window = sg.Window('My To-Do App',layout=[[[label], [input_box,add_button],[teste]]])
-window.read()
+
+window = sg.Window('My To-Do App',
+                   layout=[[[label], [input_box,add_button]]], 
+                   font=('Helvetica',10))
+
+while True:
+    event,values= window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            app.list_todos()
+            
+
 window.close()
